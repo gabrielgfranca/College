c38d5204a87da52a30676debb4aa848e026f48a9
Gabriel Franca <gabrielgfranca7@gmail.com>
2025-08-28 21:25:15 -0300

    Challenge One - Week 7 and 8

---
+++ src/Lab1/Week7_8/Challenge_One/Book.java
@@ -0,0 +1,47 @@
+package Lab1.Week7_8.Challenge_One;
+
+public class Book {
+    private String title;
+    private String author;
+    private int publishYear;
+    private double price;
+    private int pageAmount;
+
+    public Book(String title, String author, int publishYear, double price, int pageAmount) {
+        this.title = title;
+        this.author = author;
+        this.publishYear = publishYear;
+        this.price = price;
+        this.pageAmount = pageAmount;
+    }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public int getPublishYear() { return publishYear; }
+    public void setPublishYear(int publishYear) { this.publishYear = publishYear; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    public int getPageAmount() { return pageAmount; }
+    public void setPageAmount(int pageAmount) { this.pageAmount = pageAmount; }
+
+    public double pricePerPage() {
+        if (this.pageAmount > 0)
+            return price / pageAmount;
+        else
+            return 0.0;
+    }
+
+    public void printInformation() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Publish Year: " + publishYear);
+        System.out.println("Price: " + price);
+        System.out.println("Page Amount: " + pageAmount);
+    }
+}
---
+++ src/Lab1/Week7_8/Challenge_One/Client.java
@@ -0,0 +1,47 @@
+package Lab1.Week7_8.Challenge_One;
+
+public class Client {
+    private String name;
+    private int age;
+    private char gender;
+    private String email;
+    private String password;
+    private Book book;
+
+    public Client(String name, int age, char gender, String email, String password, Book book) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.email = email;
+        this.password = password;
+        this.book = book;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    public char getGender() { return gender; }
+    public void setPublishYear(char gender) { this.gender = gender; }
+
+    public String getEmail() { return email; }
+    public void setPrice(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public Book getBook() { return book; }
+    public void setBook(Book book) { this.book = book; }
+
+    public void printInformation() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+        System.out.println("Email: " + email);
+        System.out.println("Password: " + password);
+        System.out.println("Book Information's:");
+        book.printInformation();
+    }
+}
